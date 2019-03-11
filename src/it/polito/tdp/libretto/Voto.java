f90f6d495059ff3ef25b5ac2e62c7c43eeca93f7
isa <isa@fe80:0:0:0:ec1c:ceb9:40c0:61cd%12>
2019-03-11 13:54:32 +0100

    aggiunti metodi standard

---
+++ src/it/polito/tdp/libretto/Voto.java
@@ -0,0 +1,51 @@
+package it.polito.tdp.libretto;
+
+import java.time.*;
+
+public class Voto {
+
+	private int voto;
+	private String nomeCorso;
+	private LocalDate data;  // oggetti date e calender per gestire le date 
+	
+	
+	public Voto(int voto, String nomeCorso, LocalDate data) {
+		
+		this.voto = voto;
+		this.nomeCorso = nomeCorso;
+		this.data = data;
+	}
+	
+	public int getVoto() {
+		return voto;
+	}
+	
+	
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	
+	
+	public String getNomeCorso() {
+		return nomeCorso;
+	}
+	
+	
+	public void setNomeCorso(String nomeCorso) {
+		this.nomeCorso = nomeCorso;
+	}
+	
+	
+	public LocalDate getData() {
+		return data;
+	}
+	
+	
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+	
+	
+}
